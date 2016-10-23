b315fd00c0968a03e9bcc20f38533d8de6048608
Scott Crosby <scott@sacrosby.com>
2016-10-23 07:45:22 -0500

    Cleanup + OTHERS. REBASE.

--- crosby_badge.ino
+++ crosby_badge.ino
@@ -80,7 +80,7 @@ public:
     isChanged = true;
     for (int i = 0 ; i < LED_COUNT ; i++) {
       (color+i)->set(r,g,b);    
-  }
+    }
   }
 
   void set(int x, int y, int r, int g, int b) {
@@ -98,13 +98,13 @@ public:
     for (int ii = 0 ; ii < 8 ; ii++) {
       int mask = 1 << ii;
       if ((val & mask) != 0)
-	    cell(row,ii)->set(r,g,b);
+	cell(row,ii)->set(r,g,b);
     }
   }
 
 
   void draw(boolean force) {
-      const uint32_t now = millis();
+    const uint32_t now = millis();
     if (!force && (!isChanged || now-last_draw < 40))
       return;
     last_draw = now;
@@ -138,7 +138,7 @@ public:
 };
 
 class TheButton {
-  public:
+public:
   boolean nowDown = false;
   // If this is true, you can get durationDown.
   boolean justCameUp = false;
@@ -171,50 +171,69 @@ private:
 
 #define ACCEL_EMWA (1<<1)
 
-  class Accel {
-    MMA8653 chip;
-public:
-    // Some notes:
-    // 1 g is about 16 as measured by this.
-    // az is negative 16.
-    
-    // Accelerometer values at last read. Note, 45 degree orientation: '-x' is with twosigma down and on right.
-    float ax, ay, az;
-    // EMWA values.
-    float ex, ey, ez;
-    // EMWA, but displaced to the speed in x and y.
-    // dz is about -16 with 'two sigma' text on bottom.
-    // 'x' measures rows. 'y' measures columns.
-    float dx, dy;
-      // Shock accel value (detect bump)
-      float accel;
-
-    float emwa(float acc, float in) {
-      return (acc*(ACCEL_EMWA-1)+in)/ACCEL_EMWA;
-	}
-
-    void setup() {
-      // Accelerometer setup.
-      Wire.begin(i2c_sda, i2c_scl);
-      chip.setup();
+class AccelRaw {
+  float x, y, z;
+  void update(x,y,z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+  }
+}
+  class AccelEMWA {
+    const float ACCEL_EMWA = (1<<4);
+    float x, y, z;
+    void update(x,y,z) {
+      emwa(this->x,x);
+      emwa(this->y,y);
+      emwa(this->z,z);
     }
-
-    // Invoke each iteration.
-    void loop() {
-      chip.getXYZ(ax,ay,az);
-      ex=emwa(ex,ax);
-      ey=emwa(ey,ay);
-      ez=emwa(ez,az);
-      accel = sqrt(ex*ex+ey*ey+ez*ez);
-
-      // From the diagram, sensor assumes 45 degree mounting. Rotate it.
-        const float mag = sqrt(ex*ex+ey*ey);
-        const float angle = atan2(ex, -ey);
-        dx = -sin(angle + 45 * M_PI/180) * mag;
-        dy = cos(angle + 45 * M_PI/180) * mag;
+    float emwa(float &acc, float in) {
+      acc = (acc*(ACCEL_EMWA-1)+in)/ACCEL_EMWA;
     }
-  
   };
+
+class Accel {
+  MMA8653 chip;
+public:
+  // Some notes:
+  // 1 g is about 16 as measured by this.
+  // az is negative 16.
+    
+  // Accelerometer values at last read. Note, 45 degree orientation: '-x' is with twosigma down and on right.
+  float ax, ay, az;
+  // EMWA values of above. 
+  float ex, ey, ez;
+  // EMWA, but displaced to the speed in x and y.
+  // dz is about -16 with 'two sigma' text on bottom.
+  // 'x' measures rows. 'y' measures columns.
+  float dx, dy;
+  // Shock accel value (detect bump)
+  float accel;
+
+
+
+  void setup() {
+    // Accelerometer setup.
+    Wire.begin(i2c_sda, i2c_scl);
+    chip.setup();
+  }
+
+  // Invoke each iteration.
+  void loop() {
+    chip.getXYZ(ax,ay,az);
+    ex=emwa(ex,ax);
+    ey=emwa(ey,ay);
+    ez=emwa(ez,az);
+    accel = sqrt(ex*ex+ey*ey+ez*ez);
+
+    // From the diagram, sensor assumes 45 degree mounting. Rotate it.
+    const float mag = sqrt(ex*ex+ey*ey);
+    const float angle = atan2(ex, -ey);
+    dx = -sin(angle + 45 * M_PI/180) * mag;
+    dy = cos(angle + 45 * M_PI/180) * mag;
+  }
+  
+};
 
 
 Board board = Board();
@@ -232,45 +251,45 @@ public:
 
 // Show clock, accel, and switch parameters.
 class OrientationDemo : public Demo {
-  public:
+public:
   virtual void setup() {};
   virtual void loop() {
-          const uint32_t now = millis();
+    const uint32_t now = millis();
 
-      board.clear(0,0,16);
-      board.setBits(0,now >> 9,0,0,254); 
-      //board.set(5,5,255,0,0);
-      //board.set(6,6,0,255,0);
-      //board.set(7,7,0,0,255);
+    board.clear(0,0,16);
+    board.setBits(0,now >> 9,0,0,254); 
+    //board.set(5,5,255,0,0);
+    //board.set(6,6,0,255,0);
+    //board.set(7,7,0,0,255);
 
-      showAccelNicely(2,8*accel.ex);
-      showAccelNicely(3,8*accel.ey);
-      showAccelNicely(4,8*accel.ez);
-      board.setBits(5,board.brightness,255,255,255);
-      showAccelNicely(6,8*accel.dx);
-      showAccelNicely(7,8*accel.dy);
+    showAccelNicely(2,8*accel.ex);
+    showAccelNicely(3,8*accel.ey);
+    showAccelNicely(4,8*accel.ez);
+    board.setBits(5,board.brightness,255,255,255);
+    showAccelNicely(6,8*accel.dx);
+    showAccelNicely(7,8*accel.dy);
 
-      if (button.nowDown) 
-         board.set(1,0,255,128,128);
-      else
-         board.set(1,0,128,255,128);
-      if (button.justCameUp)
-         board.set(1,1,255,128,128);
-      else
-         board.set(1,1,128,255,128);
+    if (button.nowDown) 
+      board.set(1,0,255,128,128);
+    else
+      board.set(1,0,128,255,128);
+    if (button.justCameUp)
+      board.set(1,1,255,128,128);
+    else
+      board.set(1,1,128,255,128);
 
-      board.setBits(1,(button.durationDown>>6)<<2,255,255,255); 
+    board.setBits(1,(button.durationDown>>6)<<2,255,255,255); 
 
     
-    }  
+  }  
 
 
-void showAccelNicely(int col, float accel) {
-  if (accel > 0)
-        board.setBits(col,accel,0,255,0);
-  else
-        board.setBits(col,-accel,255,0,0);
-}
+  void showAccelNicely(int col, float accel) {
+    if (accel > 0)
+      board.setBits(col,accel,0,255,0);
+    else
+      board.setBits(col,-accel,255,0,0);
+  }
 };
 
 // Sliders track 16 bits internally, even though they only output/store 8 bits. That lets them handle variable accelerations.
@@ -280,26 +299,26 @@ class Slider {
   boolean wrap;
   int currentVal;
 public:
-// Should we wrap when we get to max/min?
+  // Should we wrap when we get to max/min?
   Slider(uint8_t minVal, uint8_t maxVal, uint8_t currentVal, boolean wrap) {
     this->minVal = minVal << 8;
     this->maxVal = (maxVal << 8) + 255;
     this->currentVal = currentVal<<8 + 128;
     this->wrap = wrap;
-}
+  }
   // delta is a fixedpoint with 8 fractional bits.
   void accountForDelta(int delta) {
     int maxChange = maxVal-minVal;
     if (delta > maxChange) delta = maxChange;
     if (delta < -maxChange) delta = -maxChange;
-     currentVal = currentVal + delta;
-     if (!wrap) {
+    currentVal = currentVal + delta;
+    if (!wrap) {
       if (currentVal < minVal) currentVal = minVal;
       if (currentVal > maxVal) currentVal = maxVal;
-     } else {
+    } else {
       if (currentVal < minVal) currentVal += maxChange;
       if (currentVal > maxVal) currentVal -= maxChange;
-     }
+    }
   }
   uint8_t value() {
     return currentVal>>8;
@@ -310,12 +329,12 @@ public:
 
 class PickBrightness : public Demo {
   Slider brightness = Slider(1,255,board.brightness,false);
-  public:
+public:
   virtual void setup() {};
   virtual void loop() {
-      //board.setBits(0,board.brightness,255,255,255);
-      brightness.accountForDelta(accel.dy*64);
-      board.setBrightness(brightness.value());
+    //board.setBits(0,board.brightness,255,255,255);
+    brightness.accountForDelta(accel.dy*64);
+    board.setBrightness(brightness.value());
   }
 
   
@@ -340,30 +359,30 @@ void setup() {
   board.setBrightness(64);
 }
 
-    void loop() {
-    const uint32_t now = millis();
-      accel.loop();
-      button.loop();
-      orientationDemo.loop();
-      pickBrightness.loop();
-      board.draw(false);
-      delay(20);
-    }
-    /*
-      void loopTEMP() {
+void loop() {
+  const uint32_t now = millis();
+  accel.loop();
+  button.loop();
+  orientationDemo.loop();
+  pickBrightness.loop();
+  board.draw(false);
+  delay(20);
+}
+/*
+  void loopTEMP() {
   
-      const uint32_t now = millis();
+  const uint32_t now = millis();
 
-      // only draw the LEDs at 30Hz
-      if (!do_draw && now - last_draw_millis < 30)
-      return;
-      last_draw_millis = now;
+  // only draw the LEDs at 30Hz
+  if (!do_draw && now - last_draw_millis < 30)
+  return;
+  last_draw_millis = now;
   
-      if (draw_video)
-      pixels.draw(badge.matrix);
-      else
-      demo->draw(badge.matrix);
+  if (draw_video)
+  pixels.draw(badge.matrix);
+  else
+  demo->draw(badge.matrix);
 
-      badge.matrix.show();
-      }
-    */
+  badge.matrix.show();
+  }
+*/
